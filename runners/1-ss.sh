eeb7004a3a85bd382f49e5a6b75d28f50edac9a6
lucasdavid <1595030+lucasdavid@users.noreply.github.com>
2023-11-04 17:47:48 -0300

    Update runners

--- runners/1-ss.sh
+++ runners/1-ss.sh
@@ -4,7 +4,7 @@
 #SBATCH -p sequana_gpu_shared
 #SBATCH -J ss-train
 #SBATCH -o /scratch/lerdl/lucas.david/experiments/logs/ss/train-%j.out
-#SBATCH --time=24:00:00
+#SBATCH --time=52:00:00
 
 ##SBATCH -p sequana_gpu_shared
 ##SBATCH --ntasks-per-node=48
@@ -188,25 +188,26 @@ evaluate_priors() {
 }
 
 
-LR=0.007  # voc12
+LR=0.007
 MODE=fix
 TRAINABLE_STEM=false
-TRAINABLE_BONE=true
+TRAINABLE_BONE=false
 ARCHITECTURE=resnest101
 ARCH=rs101
 
 EPOCHS=30
-MAX_STEPS=145  # 1464 (voc12 train samples) // 16 = 91 steps.
+MAX_STEPS=74  # 1464 (voc12 train samples) // 16 = 91 steps.
 BATCH_SIZE=20
 ACCUMULATE_STEPS=1
-LABELSMOOTHING=0
-AUGMENT=none  # none for DeepGlobe
+LABELSMOOTHING=0.1
+# AUGMENT=colorjitter # none for DeepGlobe
+AUGMENT=cutmix
 
 S2C_MODE=mp
 S2C_SIGMA=0.50   # min pixel confidence (conf_p := max_class(prob)_pixel >= S2C_SIGMA)
 WARMUP_EPOCHS=1  # min pixel confidence (conf_p := max_class(prob)_pixel >= S2C_SIGMA)
 C2S_SIGMA=0.75   # min pixel confidence (conf_p := max_class(prob)_pixel >= S2C_SIGMA)
-C2S_MODE=gt
+C2S_MODE=cam
 
 EID=r1  # Experiment ID
 
@@ -216,12 +217,12 @@ EID=r1  # Experiment ID
 # RESTORE=experiments/models/puzzle/ResNeSt50@Puzzle@optimal.pth
 RESTORE=experiments/models/puzzle/ResNeSt101@Puzzle@optimal.pth
 # RESTORE=experiments/models/vanilla/voc12-rn101-lr0.1-rals-r1.pth
-# RESTORE=experiments/models/pnoc/voc12-rs269-pnoc-b16-lr0.1-ls@rs269-lsra-r4.pth
+# RESTORE=experiments/models/pnoc/voc12-rs269-pnoc-b16-lr0.1-ls@rs269-rals-r4.pth
 # RESTORE=experiments/models/vanilla/deepglobe-rs50fe-rals-ce-lr0.01-cwnone-r1.pth
 # RESTORE=experiments/models/vanilla/deepglobe-rn101-lr0.1-ra-r1.pth
 # RESTORE=experiments/models/vanilla/deepglobe-rn101fe-lr0.1-ra-r1.pth
 
-TAG=ss/$DATASET-${ARCH}-lr${LR}-reco-aug-$EID
+TAG=ss/$DATASET-${ARCH}-lr${LR}-reco-wsss-w_s2c0.1-$AUGMENT-ls-$EID
 train_ss
 
 # # DOMAIN=$DOMAIN_TRAIN inference_priors
